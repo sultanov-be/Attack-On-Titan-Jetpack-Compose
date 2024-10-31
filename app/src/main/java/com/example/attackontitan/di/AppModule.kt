aa249498d3cf3155f9c5cc7bdc162f71bb0d5c5c
Bektur Sultanov <sultanov_b@auca.kg>
2024-10-31 12:24:52 +0600

    Add retrofit instance.

--- app/src/main/java/com/example/attackontitan/di/AppModule.kt
+++ app/src/main/java/com/example/attackontitan/di/AppModule.kt
@@ -1,11 +1,26 @@
 package com.example.attackontitan.di
 
+import com.example.attackontitan.data.service.ApiService
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+private const val BASE_URL = "https://api.attackontitanapi.com/"
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
-    // TODO: Implement dependent objects
+    @Singleton
+    @Provides
+    fun provideApi(): ApiService = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(OkHttpClient.Builder().build())
+        .build()
+        .create(ApiService::class.java)
 }
