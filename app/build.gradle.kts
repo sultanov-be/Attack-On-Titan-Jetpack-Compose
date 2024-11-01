a15bc1bcf69e95a330adc2a1f3ffa9bbb1d09d1c
Bektur Sultanov <sultanov_b@auca.kg>
2024-11-01 16:42:20 +0600

    Update versions of libs.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.example.attackontitan"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.attackontitan"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/example/attackontitan/activities/MainActivity.kt
+++ app/src/main/java/com/example/attackontitan/activities/MainActivity.kt
@@ -32,7 +32,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
-import coil.compose.rememberAsyncImagePainter
 import com.example.attackontitan.R
 import com.example.attackontitan.data.model.TitanBaseInfo
 import com.example.attackontitan.ui.theme.AttackOnTitanTheme
