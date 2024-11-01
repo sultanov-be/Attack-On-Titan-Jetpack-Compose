1d422befbbddcc2b28c230d5ff5e25b2a65d697b
Bektur Sultanov <sultanov_b@auca.kg>
2024-11-01 14:35:20 +0600

    Added Response body data class.

---
+++ app/src/main/java/com/example/attackontitan/data/model/Info.kt
@@ -0,0 +1,8 @@
+package com.example.attackontitan.data.model
+
+data class Info(
+    val count: Int,
+    val pages: Int,
+    val next_page: String?,
+    val prev_page: String?
+)
---
+++ app/src/main/java/com/example/attackontitan/data/model/TitanResponse.kt
@@ -0,0 +1,6 @@
+package com.example.attackontitan.data.model
+
+data class TitanResponse(
+    val info: Info,
+    val results: List<TitanBaseInfo>
+)
