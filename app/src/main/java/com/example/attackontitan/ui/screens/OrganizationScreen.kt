3b6fd1c8ddee8023bc68e0a0534025e49326a6f9
Bektur Sultanov <sultanov_b@auca.kg>
2024-11-02 14:12:39 +0600

    Add Screens.

---
+++ app/src/main/java/com/example/attackontitan/ui/screens/OrganizationScreen.kt
@@ -0,0 +1,9 @@
+package com.example.attackontitan.ui.screens
+
+import androidx.compose.runtime.Composable
+import com.example.attackontitan.ui.views.SampleScreen
+
+@Composable
+fun OrganizationScreen() {
+    SampleScreen("Organization Screen")
+}
