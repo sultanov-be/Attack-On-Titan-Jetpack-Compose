05158e19f0f71281a90f88057d00228b76c74fa6
Bektur Sultanov <sultanov_b@auca.kg>
2024-11-05 13:22:31 +0600

    Simply styled details screen.

--- app/src/main/java/com/example/attackontitan/ui/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/example/attackontitan/ui/screens/details/DetailsScreen.kt
@@ -1,18 +1,61 @@
 package com.example.attackontitan.ui.screens.details
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.attackontitan.R
+import com.example.attackontitan.ui.views.details_components.ComplicatedDetailsItem
+import com.example.attackontitan.ui.views.details_components.SimpleDetailsItem
 
 @Composable
 fun DetailsScreen(itemId: String) {
-    Text(
-        text = itemId,
-        modifier = Modifier.fillMaxWidth(),
-        fontSize = 32.sp,
-        textAlign = TextAlign.Center
-    )
+    Column(
+        modifier = Modifier
+            .padding(top = 30.dp)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Titan",
+            fontSize = 32.sp,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+        Card(
+            modifier = Modifier.size(200.dp),
+            shape = CircleShape
+        ) {
+            Image(
+                painter = painterResource(R.drawable.img),
+                contentDescription = null,
+                contentScale = ContentScale.Crop
+            )
+        }
+        SimpleDetailsItem(title = "Height", content = "15m")
+        SimpleDetailsItem(title = "Current inheritor", content = "Eron")
+        SimpleDetailsItem(title = "allegiance", content = "Eldia")
+        ComplicatedDetailsItem(false)
+        ComplicatedDetailsItem(true)
+    }
+}
+
+@Composable
+@Preview
+fun PreviewDetailsScreen() {
+    DetailsScreen("1")
 }
---
+++ app/src/main/java/com/example/attackontitan/ui/views/details_components/ComplicatedDetailsItem.kt
@@ -0,0 +1,62 @@
+package com.example.attackontitan.ui.views.details_components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.attackontitan.R
+
+@Composable
+fun ComplicatedDetailsItem(initialState: Boolean) {
+    var isShown by remember {
+        mutableStateOf(initialState)
+    }
+
+    Column(
+        modifier = Modifier.fillMaxWidth().padding(5.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Abilities",
+                fontSize = 18.sp
+            )
+
+            Image(
+                painter = painterResource(R.drawable.arrow_down_img),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(40.dp)
+                    .padding(start = 10.dp)
+                    .clickable {
+                        isShown = !isShown
+                    },
+            )
+        }
+        if (isShown) {
+            Column {
+                Text(text = "Eron")
+                Text(text = "Beka")
+                Text(text = "Sule")
+            }
+        }
+    }
+}
