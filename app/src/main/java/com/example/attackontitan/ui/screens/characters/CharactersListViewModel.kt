b54cc279ad9ce92f4217e269b7f387f2cf355355
Bektur Sultanov <sultanov_b@auca.kg>
2024-11-25 17:51:35 +0600

    Integrated to UI layer.

---
+++ app/src/main/java/com/example/attackontitan/ui/screens/characters/CharactersListViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.attackontitan.ui.screens.characters
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.example.attackontitan.data.model.CharacterBaseInfo
+import com.example.attackontitan.data.repository.characters.CharactersListRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+@HiltViewModel
+class CharactersListViewModel @Inject constructor(
+    private val repository: CharactersListRepository
+) : ViewModel() {
+
+    val charactersPagingData: Flow<PagingData<CharacterBaseInfo>> =
+        repository.getCharacters().cachedIn(viewModelScope)
+
+}
