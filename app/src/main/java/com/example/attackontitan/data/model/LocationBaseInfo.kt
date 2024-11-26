27119b0f4c22987c9601308fff79b85c4c826629
Bektur Sultanov <sultanov_b@auca.kg>
2024-11-26 11:08:04 +0600

    Add data model and api interface.

---
+++ app/src/main/java/com/example/attackontitan/data/model/LocationBaseInfo.kt
@@ -0,0 +1,9 @@
+package com.example.attackontitan.data.model
+
+data class LocationBaseInfo(
+    val id: Int,
+    val name: String,
+    val img: String,
+    val territory: String,
+    val region: String
+)
---
+++ app/src/main/java/com/example/attackontitan/data/service/LocationsListApiService.kt
@@ -0,0 +1,13 @@
+package com.example.attackontitan.data.service
+
+import com.example.attackontitan.data.model.ApiResponse
+import com.example.attackontitan.data.model.LocationBaseInfo
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface LocationsListApiService {
+    @GET
+    suspend fun getLocations(
+        @Query("page")page: Int
+    ): ApiResponse<LocationBaseInfo>
+}
