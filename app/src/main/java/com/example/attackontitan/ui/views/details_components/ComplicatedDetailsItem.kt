55c167689b1cc44eec90c57242f22b744d671823
Bektur Sultanov <sultanov_b@auca.kg>
2024-11-26 14:24:14 +0600

    Redo some views related to details.

--- app/src/main/java/com/example/attackontitan/ui/views/details_components/ComplicatedDetailsItem.kt
+++ app/src/main/java/com/example/attackontitan/ui/views/details_components/ComplicatedDetailsItem.kt
@@ -1,7 +1,6 @@
 package com.example.attackontitan.ui.views.details_components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,10 +11,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -26,12 +21,10 @@ import com.example.attackontitan.utils.firstToCapital
 
 @Composable
 fun ComplicatedDetailsItem(title: String, list: List<String>) {
-    var isShown by remember {
-        mutableStateOf(false)
-    }
-
     Column(
-        modifier = Modifier.fillMaxWidth().padding(5.dp)
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(5.dp)
     ) {
         Row(
             modifier = Modifier.fillMaxWidth(),
@@ -49,16 +42,11 @@ fun ComplicatedDetailsItem(title: String, list: List<String>) {
                 modifier = Modifier
                     .size(40.dp)
                     .padding(start = 10.dp)
-                    .clickable {
-                        isShown = !isShown
-                    },
             )
         }
-        if (isShown) {
-            LazyColumn {
-                items(list) { items ->
-                    Text(text = items)
-                }
+        LazyColumn {
+            items(list) { items ->
+                Text(text = items)
             }
         }
     }
