1306888bd11b60d1e8064e3f35f6c933f2224ad2
sultanov-be <70717785+sultanov-be@users.noreply.github.com>
2024-11-27 13:35:59 +0600

    Revert "feature/ui_part"

--- app/src/main/java/com/example/attackontitan/data/model/locations/LocationBaseInfo.kt
+++
@@ -1,9 +0,0 @@
-package com.example.attackontitan.data.model.locations
-
-data class LocationBaseInfo(
-    val id: Int,
-    val name: String,
-    val img: String,
-    val territory: String,
-    val region: String
-)
