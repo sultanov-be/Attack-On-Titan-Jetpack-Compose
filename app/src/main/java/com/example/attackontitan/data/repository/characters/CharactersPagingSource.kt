27c51a23972b834f109edcbae4f64ae2c0a62583
Bektur Sultanov <sultanov_b@auca.kg>
2024-11-28 12:53:08 +0600

    Remove irrelevant todo.

--- app/src/main/java/com/example/attackontitan/data/repository/characters/CharactersPagingSource.kt
+++ app/src/main/java/com/example/attackontitan/data/repository/characters/CharactersPagingSource.kt
@@ -4,7 +4,6 @@ import com.example.attackontitan.data.model.characters.CharacterBaseInfo
 import com.example.attackontitan.data.repository.BasePagingSource
 import com.example.attackontitan.data.service.CharacterApiService
 
-//(TODO: Maybe edited later)
 class CharactersPagingSource(
     private val apiService: CharacterApiService,
     private val query: String? = null
